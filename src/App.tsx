341550b4c5d0028049af6c1c85ad125ed0b17a43
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-12-29 16:14:47 -0500

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Editor, { Monaco } from "@monaco-editor/react";
+import { Box, Typography } from "@mui/material";
+import RunButton from "./RunButton";
+import * as monaco from "monaco-editor";
 
 function App() {
+  const [content, setContent] = useState<string | undefined>(
+    "for(let i = 0; i < 10; i++) console.log(Math.random());"
+  );
+  const [output, setOutput] = useState("");
+  const run = () => {
+    setOutput("");
+    const preCode = `
+console.log = (...args) => { self.postMessage({type: "log", args: args})};
+`;
+    const code = content ?? "";
+    const postCode = `
+self.postMessage({type: "terminate"})`;
+    const fullCode = preCode + code + postCode;
+    const blob = new Blob([fullCode], {
+      type: "text/javascript",
+    });
+    const url = URL.createObjectURL(blob);
+    const w = new Worker(url);
+    w.onerror = (ev) => {
+      console.log(ev);
+    };
+    w.onmessage = (ev) => {
+      if (ev.data.type === "terminate") {
+        w.terminate();
+        URL.revokeObjectURL(url);
+        return;
+      }
+
+      if (ev.data.type === "log") {
+        setOutput((output) => output + ev.data.args.join(" ") + "\n");
+        return;
+      }
+    };
+  };
+
+  const onMount = (
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monaco: Monaco
+  ) => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Editor
+        onMount={onMount}
+        value={content}
+        onChange={(c) => setContent(c)}
+        options={{ fontSize: 25 }}
+        defaultLanguage="javascript"
+        theme={"vs-dark"}
+      />
+
+      <Box
+        sx={{ height: "100px", overflow: "scroll", backgroundColor: "#ddd" }}
+      >
+        <Typography sx={{ whiteSpace: "pre-wrap" }}>{output}</Typography>
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <RunButton onRun={run} sx={{ width: "100%" }} />
+      </Box>
+    </Box>
   );
 }
 
